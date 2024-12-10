46fdf7d6b4315ffbeaa7d61890b39d92d42e8a1f
pochiman <pochiman@yahoo.com>
2024-12-09 20:58:51 -0500

    initial commit

---
+++ lesson-09/app.py
@@ -0,0 +1,39 @@
+# booleans
+is_authenticated = True
+
+# print(is_authenticated)
+# print(not is_authenticated)
+
+# comparison operators
+x, y = 5, 10
+
+# print(x > y)
+# print(x < y)
+# print(x == y)
+# print(x != y)
+# print(x >= y)
+# print(x <= y)
+
+# boolean operators & member checking
+x, y = True, False
+
+# print(x and y)
+# print(x or y)
+
+# falsy values --> 0 numbers and empty data structs
+# print(bool(0))
+# print(bool(""))
+# print(bool([]))
+
+# truthy values --> everything else
+# print(bool(1))
+# print(bool("hello, ninjas"))
+# print(bool([100, 99, 98]))
+
+
+# evaluating strings
+name = "mario"
+
+print(name.startswith('m'))
+print(name.startswith('a'))
+print(name.endswith('o'))
