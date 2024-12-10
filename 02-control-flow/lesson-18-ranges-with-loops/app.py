63431d9c2c6534fad46283d23ab440b2dd7b7eb9
pochiman <pochiman@yahoo.com>
2024-12-10 09:16:54 -0500

    initial commit

---
+++ 02-control-flow/lesson-18-ranges-with-loops/app.py
@@ -0,0 +1,18 @@
+# for loops with ranges
+
+for i in range(5):
+  print(i)
+
+for i in range(10, 20):
+  print(i)
+
+for i in range(10, 20, 2):
+  print(i)
+
+# negative steps
+
+for i in range(10, 0, -1):
+  print(i)
+
+for i in range(20, 10, -2):
+  print(i)
