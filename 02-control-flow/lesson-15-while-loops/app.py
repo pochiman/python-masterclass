3993e2f2b21f75e306cdb568ff98d0f4fe554b80
pochiman <pochiman@yahoo.com>
2024-12-10 08:52:47 -0500

    initial commit

---
+++ 02-control-flow/lesson-15-while-loops/app.py
@@ -0,0 +1,13 @@
+# while loops
+
+# score = 0
+
+# while score < 10:
+#   print('the score is:', score)
+#   score += 1
+
+user_input = ""
+
+while user_input != "q":
+  user_input = input("Enter a letter or enter 'q' to quit: ")
+  print('You entered:', user_input)
