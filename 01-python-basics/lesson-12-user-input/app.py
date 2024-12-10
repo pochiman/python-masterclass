fa19ec089ffc430e897c1561f81463ffe4feb7ff
pochiman <pochiman@yahoo.com>
2024-12-09 21:36:01 -0500

    initial commit

---
+++ 01-python-basics/lesson-12-user-input/app.py
@@ -0,0 +1,10 @@
+# user input
+name = input("Your name, please: ")
+age = int(input("Your age, please: "))
+
+print(f"Hello {name}, you are {age} years old")
+
+a = float(input("First number: "))
+b = float(input("Second number: "))
+
+print(f"The sum of those 2 numbers is {a + b}")
